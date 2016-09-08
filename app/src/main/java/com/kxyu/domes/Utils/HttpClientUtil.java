90b88e5d02692ca9e37eadb24ca96490a81ade35
kxyu <kxyu@lewatek.com>
2016-09-08 09:55:19 +0800

    update

--- app/src/main/java/com/kxyu/domes/Utils/HttpClientUtil.java
+++ app/src/main/java/com/kxyu/domes/Utils/HttpClientUtil.java
@@ -10,18 +10,29 @@ public class HttpClientUtil {
     private static final String XSCREEN_URL = "http://newsapi.revanow.com/api/";
     private static final String LIST_URL = "102";
     private static final String CONTENT_TYPE = "application/json";
-
+    public static final String DETAIL_URL = "articles";
 
     private static String getXscreenListUrl() {
         return XSCREEN_URL + LIST_URL;
     }
 
-    public static void useOkHttpPost(String jsonString, StringCallback callback) {
+    public static void useOkHttpPost(String jsonString, StringCallback callback, String url ) {
         OkHttpUtils
                 .postString()
-                .url(getXscreenListUrl())
+                .url(XSCREEN_URL + url)
                 .content(jsonString)
                 .build()
                 .execute(callback);
+    }
+
+    public static void useOkHttpPost(String jsonString, StringCallback callback ) {
+        useOkHttpPost(jsonString, callback, LIST_URL);
+
+    }
+
+    public static void useOkHttpGet(String Url, StringCallback callback){
+
+        OkHttpUtils.get().url(Url).build().execute(callback);
+
     }
 }
