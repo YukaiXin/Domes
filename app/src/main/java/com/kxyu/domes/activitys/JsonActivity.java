8332b19b47b54d6e3480da6d16f0c29bdeab63d6
yuki_cool <my369762419@vip.qq.com>
2016-11-22 11:28:53 +0800

    gson.fromJson 遇到的错误

--- app/src/main/java/com/kxyu/domes/activitys/JsonActivity.java
+++ app/src/main/java/com/kxyu/domes/activitys/JsonActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
 import com.kxyu.domes.R;
 import com.kxyu.domes.Utils.FileUtils;
 
@@ -27,13 +28,11 @@ public class JsonActivity extends Activity implements View.OnClickListener {
     Button writeBtn;
     Button readBtn;
     TextView tv;
-    CardData car;
-    HashMap<String, card> map = new HashMap<>();
+    HashMap<String, card> mMap = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json_layout);
-        car = new CardData();
         initMap();
         writeBtn = (Button) findViewById(R.id.writeForjson);
         readBtn = (Button) findViewById(R.id.readForjson);
@@ -44,27 +43,27 @@ public class JsonActivity extends Activity implements View.OnClickListener {
     }
 
     private void initMap(){
-        car.map.put("A", new card("Pop App", 1, 12323));
-        car.map.put("B", new card("Pop Game", 12, 3232));
-        car.map.put("C", new card("Navigation", 13, 3232));
-        car.map.put("D", new card("Good Video", 14, 333));
+        mMap.put("A", new card("Pop App", 1, 12323));
+        mMap.put("B", new card("Pop Game", 12, 3232));
+        mMap.put("C", new card("Navigation", 13, 3232));
+        mMap.put("D", new card("Good Video", 14, 333));
     }
     private void writeJson(){
         Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
         JSONObject allData = new JSONObject();
-        String s = gson.toJson(car);
-        Log.i("kxyu","string : "+s);
-        FileUtils.writeFile(this, "crad", s,true);
+        String str = gson.toJson(mMap);
+        Log.i("kxyu","string : "+str);
+        FileUtils.writeFile(this, "cardinfo", str,true);
     }
 
     private void readjson(){
-       Gson gson = new Gson();
-        String data = FileUtils.readFile(this, "crad");
-        CardData cl;
-        cl = gson.fromJson(data,CardData.class);
-        Log.i("kxyu","readfile : "+data);
-        if(cl.map != null){
-            Log.i("kxyu","readfile c1: "+cl.map.get("A").showName);
+        Gson gson = new Gson();
+        String data = FileUtils.readFile(this, "cardinfo");
+        HashMap<String, card> tmpMap;
+        tmpMap = gson.fromJson(data, new TypeToken<HashMap<String, card>>(){}.getType());
+        Log.i("kxyu","readfile : "+tmpMap);
+        if(tmpMap != null){
+            Log.i("kxyu","readfile Map: "+tmpMap.get("A").showName);
         }
     }
 
@@ -76,15 +75,6 @@ public class JsonActivity extends Activity implements View.OnClickListener {
             writeJson();
         }
     }
-public class CardData{
-
-    public CardData(){
-        this.map = new HashMap<>();
-    }
-    @SerializedName("map")
-    public HashMap<String, card> map;
-}
-
 
     public class card{
 
